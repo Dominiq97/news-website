8ce97d1aa2eff1997d1db98e098909a43fdca721
Dominic Catana <catanadominic@gmail.com>
2020-06-09 20:26:28 +0300

    base.html as master and taking from models infos

--- main/models.py
+++ main/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 class Main(models.Model):
     name = models.TextField()
+    about = models.TextField()
 
-    def __str__(self):
-        return self.name
+    
     
 
 
--- main/views.py
+++ main/views.py
@@ -4,7 +4,10 @@ from .models import Main
 # Create your views here.
 
 def home(request):
-    return render(request, 'front/home.html')
+ #   site_name = "News of Tech | HOME"
+    site = Main.objects.get(pk=2)
+    return render(request, 'front/home.html',{'sitename':site})
 
 def about(request):
-    return render(request, 'front/about.html')
+    site_name = "News of Tech | ABOUT"
+    return render(request, 'front/about.html',{'sitename':site_name})
