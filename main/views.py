d95b2117b5b3fe1536376e2ec8e37321291f86bf
Dominic Catana <catanadominic@gmail.com>
2020-06-09 20:43:43 +0300

    title of pages

--- main/views.py
+++ main/views.py
@@ -6,8 +6,8 @@ from .models import Main
 def home(request):
  #   site_name = "News of Tech | HOME"
     site = Main.objects.get(pk=2)
-    return render(request, 'front/home.html',{'sitename':site})
+    return render(request, 'front/home.html',{'site':site})
 
 def about(request):
-    site_name = "News of Tech | ABOUT"
-    return render(request, 'front/about.html',{'sitename':site_name})
+    site = Main.objects.get(pk=2)
+    return render(request, 'front/about.html',{'site':site})
