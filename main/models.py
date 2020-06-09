709ae5e458240f38e469a0045a4cbfc27e14ddf1
Dominic Catana <catanadominic@gmail.com>
2020-06-09 21:08:13 +0300

    title for records

--- main/models.py
+++ main/models.py
@@ -6,8 +6,13 @@ class Main(models.Model):
     name = models.TextField()
     about = models.TextField()
     fb = models.TextField(default="-")
-    tw = models.TextField(default="-")
+    tw = models.TextField(default="-")  
     yt = models.TextField(default="-")
+
+    set_name = models.TextField(default="-")
+
+    def __str__(self):
+        return self.set_name + " | " + str(self.pk)
     
     
 
