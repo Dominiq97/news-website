b99f62f7bf72fe5bb7422a4f64e75bae1f7dbaa1
Dominic Catana <catanadominic@gmail.com>
2020-06-10 11:54:26 +0300

    polish model Site settings

--- main/models.py
+++ main/models.py
@@ -3,13 +3,14 @@ from django.db import models
 # Create your models here.
 
 class Main(models.Model):
-    name = models.TextField()
+    name = models.CharField(max_length=60)
     about = models.TextField()
-    fb = models.TextField(default="-")
-    tw = models.TextField(default="-")  
-    yt = models.TextField(default="-")
-
-    set_name = models.TextField(default="-")
+    fb = models.CharField(default="-",max_length=30)
+    tw = models.CharField(default="-",max_length=30)
+    yt = models.CharField(default="-",max_length=30)
+    tel = models.CharField(default="-",max_length=30)
+    link = models.CharField(default="-",max_length=30)
+    set_name = models.CharField(default="-",max_length=30)
 
     def __str__(self):
         return self.set_name + " | " + str(self.pk)
