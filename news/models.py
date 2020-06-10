25f9a27426d452d20ec27c97949ac910f867a6c8
Dominic Catana <catanadominic@gmail.com>
2020-06-10 13:05:56 +0300

    arrange news in template

--- news/models.py
+++ news/models.py
@@ -6,7 +6,7 @@ class News(models.Model):
     name = models.CharField(max_length=50)
     summary = models.TextField()
     body = models.TextField()
-    name = models.CharField(max_length=12)
+    date = models.CharField(max_length=12,default="//")
     pic = models.TextField()
     writer = models.CharField(max_length=50)
 
