3c454d77c02c4a2481e861a7076b670dd5063743
Dominic Catana <catanadominic@gmail.com>
2020-06-10 13:05:56 +0300

    arrange news in template

--- main/views.py
+++ main/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Main 
+from .models import Main
+from news.models import News
 
 # Create your views here.
 
 def home(request):
  #   site_name = "News of Tech | HOME"
     site = Main.objects.get(pk=2)
-    return render(request, 'front/home.html',{'site':site})
+    news = News.objects.all()
+    return render(request, 'front/home.html',{'site':site,'news':news})
 
 def about(request):
     site = Main.objects.get(pk=2)
--- news/views.py
+++ news/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import News
 
 # Create your views here.
+
+
+
