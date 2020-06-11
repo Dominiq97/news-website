caf96c93aa091b25ea6ee9f7e7d85f606ef5ff62
Dominic Catana <catanadominic@gmail.com>
2020-06-12 02:23:41 +0300

    set-up for upload files

--- myproject/urls.py
+++ myproject/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.conf.urls import include, url
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('main.urls')),
     url(r'',include('news.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
