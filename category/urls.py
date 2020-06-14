4f62ac65c94a107815c6eb4e9cefd05bff22e234
Dominic Catana <catanadominic@gmail.com>
2020-06-14 15:56:29 +0300

    category template

--- category/urls.py
+++ category/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    
+    url(r'^panel/category/list/$',views.category_list, name='category_list'),
 ]
--- category/views.py
+++ category/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Category
 
 # Create your views here.
+
+def category_list(request):
+    category = Category.objects.all()
+    return render(request,'back/category_list.html',{'category':category})
