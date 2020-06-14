41c0df8dcf9c73515fcb443fec75438074c0c90f
Dominic Catana <catanadominic@gmail.com>
2020-06-14 16:32:06 +0300

    add category

--- category/views.py
+++ category/views.py
@@ -6,3 +6,19 @@ from .models import Category
 def category_list(request):
     category = Category.objects.all()
     return render(request,'back/category_list.html',{'category':category})
+
+def category_add(request):
+    if request.method =='POST':
+        catname=request.POST.get('catname')
+        if catname == "":
+            error = "All fields required"
+            return render(request,'back/error.html',{'error':error})
+        if len(Category.objects.filter(name=catname)) != 0 :
+            error = "This Name Used Before!"
+            return render(request,'back/error.html',{'error':error})
+        category = Category(name=catname)
+        category.save()
+        return redirect('category_list')
+
+
+    return render(request,'back/category_add.html')
