a4eb19a15d5451cabe43e89765157cb3f9f14939
Dominic Catana <catanadominic@gmail.com>
2020-06-23 01:07:19 +0300

    live markdown

--- myproject/settings.py
+++ myproject/settings.py
@@ -32,9 +32,9 @@ MARTOR_ENABLE_CONFIGS = getattr(
     settings, 'MARTOR_ENABLE_CONFIGS', {
         'emoji': 'true',        # to enable/disable emoji icons.
         'imgur': 'true',        # to enable/disable imgur/custom uploader.
-        'mention': 'false',     # to enable/disable mention
+        'mention': 'true',     # to enable/disable mention
         'jquery': 'true',       # to include/revoke jquery (require for admin default django)
-        'living': 'false',      # to enable/disable live updates in preview
+        'living': 'true',      # to enable/disable live updates in preview
         'spellcheck': 'false',  # to enable/disable spellcheck in form textareas
         'hljs': 'true',         # to enable/disable hljs highlighting in preview
     }
