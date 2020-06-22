c4fba80c4c051114f1d07f1a7b35765d1e7f246b
Dominic Catana <catanadominic@gmail.com>
2020-06-22 16:05:32 +0300

    edit subcategory complete

--- subcategory/urls.py
+++ subcategory/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^panel/subcategory/list/$',views.subcategory_list, name='subcategory_list'),
     url(r'^panel/subcategory/add/$',views.subcategory_add, name='subcategory_add'),
     url(r'^panel/subcategory/delete/(?P<pk>\d+)/$',views.subcategory_delete, name='subcategory_delete'),
+    url(r'^panel/subcategory/edit/(?P<pk>\d+)/$',views.subcategory_edit, name='subcategory_edit'),
 ]
--- subcategory/views.py
+++ subcategory/views.py
@@ -35,3 +35,36 @@ def subcategory_delete(request,pk):
         error = "Something Wrong"
         return render(request,'back/error.html',{'error':error})
     return redirect('subcategory_list')
+
+def subcategory_edit(request,pk):
+
+    if len(SubCategory.objects.filter(pk=pk))==0:
+        error = "Category does not exist!"
+        return render(request,'back/error.html',{'error':error})
+    subcategory = SubCategory.objects.get(pk=pk)
+    category = Category.objects.all()
+
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        catname = request.POST.get('category')
+        catid = request.POST.get('category')
+        if name == "":
+            error = "All fields required"
+            return render(request,'back/error.html',{'error':error})
+        try:
+            subcategory_category = Category.objects.get(pk=catid).name
+            subcategory_edited = SubCategory.objects.get(pk=pk)
+            subcategory_edited.name=name
+            subcategory_edited.catname=subcategory_category
+            subcategory_edited.catid=catid
+            subcategory_edited.save()
+            return redirect('subcategory_list')
+                    
+        except:
+            subcategory_edited = SubCategory.objects.get(pk=pk)
+            subcategory_edited.name=name
+            subcategory_edited.catname=catname
+            subcategory_edited.catid=catid
+            subcategory_edited.save()
+            return redirect('subcategory_list')
+    return render(request, 'back/subcategory_edit.html',{'pk':pk,'subcategory':subcategory,'category':category})
