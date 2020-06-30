cf33b4f176c1c71259efa2c19f5ce3ff38b5e1f3
Dominic Catana <catanadominic@gmail.com>
2020-06-30 16:40:35 +0300

    schimbare nr maxim char model titlu

--- news/models.py
+++ news/models.py
@@ -12,7 +12,7 @@ class Tag(models.Model):
 		return(self.name)
 
 class News(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=250)
     summary = models.TextField()
     body = MartorField()
     date = models.CharField(max_length=12,default="//")
